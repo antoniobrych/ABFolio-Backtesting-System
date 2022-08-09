fe3ac546c78e1f185c8517556909bbc14ea20d3c
antoniobrych <63557494+antoniobrych@users.noreply.github.com>
2022-08-09 00:03:28 -0300

    Update
    Portfolio 1 user area added.
    
    Total Portfolio available percentage added.

--- abfolio.py
+++ abfolio.py
@@ -7,7 +7,6 @@ isin_history2 = []
 sum1=1
 sum2=1
 click_count = -1
-
 #********REGISTER ALL DATA IN ISIN_ARRAY**********************
 def registerData():
     global sum1
@@ -15,24 +14,34 @@ def registerData():
     global click_count
     global isin_history
     global isin_history2
-    click_count +=1
-    if round(sum1,2)>0:
-        history_label = Label(root)
-        quote = inv.search_quotes(text=asset_name.get().upper(),products = [asset_box.get().lower()],n_results = 1)
-        isin_history.append([quote.name,quote.symbol,quote.country,round(float(asset_perc.get())/100,2),asset_box.get().lower(),asset_name.get().upper()])
-        sum1 -= round((float(asset_perc.get())/100),2)
-        history_label.config(text=str(round(sum1,2)))
-        history_label.place(x=16,y=100+(20*click_count))
-        print(isin_history)
-        asset_perc.delete(0,'end')
-        asset_box.delete(0,'end')
-        asset_name.delete(0,'end')
-    if round(sum2,2)>0:  
+    if len(asset_perc.get()) > 0 and len(asset_box.get()) > 0 and len(asset_name.get())>0:
+        click_count +=1
+    if round(sum1,4)>0:
+        if len(asset_perc.get()) > 0 and len(asset_box.get()) > 0 and len(asset_name.get())>0:
+            history_label = Label(root)
+            individual_asset_label = Label(root)
+            quote = inv.search_quotes(text=asset_name.get().upper(),products = [asset_box.get().lower()],n_results = 1)
+            isin_history.append([quote.name,quote.symbol,quote.country,round(float(asset_perc.get())/100,4),asset_box.get().lower(),asset_name.get().upper()])
+            sum1 -= round((float(asset_perc.get())/100),4)
+            individual_asset_label.config(text="%s - %s | total = %f"%(quote.name,round(float(asset_perc.get()),4),round(sum1,4)*100),fg='black',font='Times 13 italic',bg="#1c87b7")
+            total_sum_label.config(text=str(round(sum1,4)*100)+"% to remaining to be allocated",fg='black',font='Times 13 italic',bg="#1c87b7")
+            individual_asset_label.place(x=500,y=70 + (50*click_count))
+            print(isin_history)
+            asset_perc.delete(0,'end')
+            asset_box.delete(0,'end')
+            asset_name.delete(0,'end')
+    elif round(sum2,4)>0:  
+        register_bttn.configure(bd=5,relief='raised',text='Click to register asset in "Portfolio 2"',fg='black')
         if len(asset_perc.get()) > 0 and len(asset_box.get()) > 0 and len(asset_name.get())>0:
             quote = inv.search_quotes(text=asset_name.get().upper(),products = [asset_box.get().lower()],n_results = 1)
-            isin_history2.append([quote.name,quote.symbol,quote.country,float(asset_perc.get())/100,asset_box.get().lower(),asset_name.get().upper()])
+            isin_history2.append([quote.name,quote.symbol,quote.country,round(float(asset_perc.get())/100,4),asset_box.get().lower(),asset_name.get().upper()])
             sum2 -= round(float(asset_perc.get())/100,4)
+            asset_perc.delete(0,'end')
+            asset_box.delete(0,'end')
+            asset_name.delete(0,'end')
         print(isin_history2)
+    else:
+        register_bttn.place_forget()
 
 #******************************************************************
 def backtestScreen():
@@ -46,9 +55,12 @@ def backtestScreen():
     ticker_label.place(x=160,y=17)
     percentage.place(x=310,y=17) 
     register_bttn.place(x=16,y=75)
+    total_sum_label.place(x=16,y=110)
+    p1_area_label.place(x=500,y=38)
+
    
 root = Tk()
-
+total_sum_label = Label(root)
 #**************SCREEN CONFIG****************
 #comprimento x altura
 root.geometry("900x500")
@@ -60,7 +72,7 @@ start_bttn.configure(bd=5,relief='raised',text='Click to Proceed')
 start_bttn.place(x=354,y=300)
 #**********MAIN MENU TITLE********************
 title = Label(root)
-title.configure(font='Consolas 32 bold',fg="gold",bg = '#1c87b7',text='ABFolio')
+title.configure(font='Consolas 32 bold',fg="black",bg = '#1c87b7',text='ABFolio')
 title.place(x=361,y=80)
 #***************COMBOBOX************
 asset_box = ttk.Combobox(root,values=assets_class,background='#1c87b7')
@@ -83,8 +95,12 @@ percentage.configure(text='Percentage:',bg='#1c87b7',fg='black')
 #**************COFNIG AND DECLARE ASSET REGISTRATION BUTTON******************
 
 register_bttn = Button(root,bg='orange',width=51,height=1,fg='white',command=registerData)
-register_bttn.configure(bd=5,relief='raised',text='Click to register asset',fg='black')
+register_bttn.configure(bd=5,relief='raised',text='Click to register asset in "Portfolio 1"',fg='black')
+#**************SUM LABEL FOR ASSET ALLOC. init screen******************
+total_sum_label.configure(text=str(round(sum1,5)*100)+"% to remaining to be allocated",fg='black',font='Consolas 13 italic',bg="#1c87b7")
+#*****************Portfolio 1 Area Label****************************
+p1_area_label = Label(root)
+p1_area_label.configure(text= 'Portfolio 1:',font='Consolas 16 bold',bg='#1c87b7')
 
 root.mainloop()
-
 
